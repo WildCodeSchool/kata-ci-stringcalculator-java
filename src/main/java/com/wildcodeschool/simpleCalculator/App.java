73ec40afb53c42ba4aef03bb581ad145b2bfdca9
bastienwcs <bastien@wldcodeschool.fr>
2020-05-20 16:59:44 +0200

    Support different delimiters

--- src/main/java/com/wildcodeschool/simpleCalculator/App.java
+++ src/main/java/com/wildcodeschool/simpleCalculator/App.java
@@ -1,13 +1,34 @@
 package com.wildcodeschool.simpleCalculator;
 
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static int add(String numbers) {
+        if (numbers == null || numbers.length() == 0) {
+            return 0;
+        }
+        String delimiter = ",";
+        if (numbers.startsWith("//")) {
+            int nl = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, nl);
+            numbers = numbers.substring(nl + 1);
+        }
+        numbers = numbers.replaceAll("\n", delimiter);
+
+        return Arrays.stream(numbers.split(delimiter))
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
     }
 }
--- src/test/java/com/wildcodeschool/simpleCalculator/AppTest.java
+++ src/test/java/com/wildcodeschool/simpleCalculator/AppTest.java
@@ -2,19 +2,26 @@ package com.wildcodeschool.simpleCalculator;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testAdd() {
+
+        Assert.assertEquals(0, App.add(""));
+        Assert.assertEquals(1, App.add("1"));
+        Assert.assertEquals(3, App.add("2,1"));
+        Assert.assertEquals(0, App.add("1,-1"));
+        Assert.assertEquals(6, App.add("3,2,1"));
+        Assert.assertEquals(6, App.add("3,2\n1"));
+        Assert.assertEquals(3, App.add("//;\n1;2"));
     }
 }
